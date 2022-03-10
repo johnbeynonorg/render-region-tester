c82295f57be4398256866cb9c6caef44c1db7883
John Beynon <john@render.com>
2022-03-10 11:52:26 +0000

    Update to allow CORS

--- return-ok/app.rb
+++ return-ok/app.rb
@@ -1,5 +1,12 @@
 # myapp.rb
 require 'sinatra'
+require 'rack/cors'
+
+use Rack::Cors do |config|
+  config.allow do |allow|
+    allow.origins '*'
+  end
+end
 
 get '/' do
   "Hello from #{ENV['REGION']}"
