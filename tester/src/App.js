62373ddd93c7f86274e328830002460f0dace73c
John Beynon <john@render.com>
2022-03-10 11:52:40 +0000

    Add new region to test

--- tester/src/App.js
+++ tester/src/App.js
@@ -5,7 +5,8 @@ import Chart from './Chart';
 function App() {
   return (
     <div className="App">
-      <Chart region="Frankfurt" url="/frankfurt" />
+      <Chart region="Frankfurt (rewrite)" url="/frankfurt" />
+      <Chart region="Frankfurt (direct)" url="https://test-frankfurt.onrender.com" />
       <Chart region="Ohio (rewrite)" url="/ohio" />
       <Chart region="Oregon (rewrite)" url="/oregon" />
       <Chart region="Singapore (rewrite)" url="/singapore" />
