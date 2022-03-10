1ea606e7e7383df93cc9fcb122cec862d6b07b7b
John Beynon <john@render.com>
2022-03-10 14:20:09 +0000

    Figure out visitor location

--- tester/src/App.js
+++ tester/src/App.js
@@ -1,9 +1,13 @@
 import Chart from './Chart';
+import useGeoLocation from "react-ipgeolocation";
+import ReactCountryFlag from "react-country-flag"
+const lookup = require('country-code-lookup')
 
 function App() {
+  const location = useGeoLocation();
   return (
     <div className="p-6">
-      <ul role="list" class="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-2">
+      <ul role="list" className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-2">
           <Chart region="Frankfurt (rewrite)" url="/frankfurt" />
           <Chart region="Frankfurt (direct)" url="https://test-frankfurt.onrender.com" />
           <Chart region="Ohio (rewrite)" url="/ohio" />
@@ -13,6 +17,7 @@ function App() {
           <Chart region="Singapore (rewrite)" url="/singapore" />
           <Chart region="Singapore (direct)" url="https://test-singapore-ak20.onrender.com" />
       </ul>
+      <p className="pt-2">Your location: {location.country} <ReactCountryFlag countryCode={location.country} /></p>
     </div>
   );
 }
