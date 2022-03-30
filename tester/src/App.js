3f7debb5f8b3b18143bc27649940a20cc4c1735f
John Beynon <john@render.com>
2022-03-30 09:29:22 +0100

    Add Frankfurt via Vercel

--- tester/src/App.js
+++ tester/src/App.js
@@ -10,14 +10,15 @@ function App() {
       <ul role="list" className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-2">
           <Chart region="Frankfurt (rewrite)" url="/frankfurt" />
           <Chart region="Frankfurt (direct)" url="https://test-frankfurt.onrender.com" />
+          <Chart region="Frankfurt NETLIFY (redirect)" url="https://624411b07063450008875bd5--extraordinary-chaja-86c0d6.netlify.app/frankfurt" />
+          <Chart region="Frankfurt VERCEL (redirect)" url="https://vercel-rewrite-rho.vercel.app/frankfurt" />        
           <Chart region="Ohio (rewrite)" url="/ohio" />
           <Chart region="Ohio (direct)" url="https://test-ohio.onrender.com" />
           <Chart region="Oregon (rewrite)" url="/oregon" />
           <Chart region="Oregon (direct)" url="https://test-oregon.onrender.com" />
           <Chart region="Singapore (rewrite)" url="/singapore" />
           <Chart region="Singapore (direct)" url="https://test-singapore-ak20.onrender.com" />
-          <Chart region="Frankfurt NETLIFY (redirect)" url="https://624411b07063450008875bd5--extraordinary-chaja-86c0d6.netlify.app/frankfurt" />
-      </ul>
+           </ul>
       <p className="pt-2">Your location: {location.country} <ReactCountryFlag countryCode={location.country} /></p>
     </div>
   );
