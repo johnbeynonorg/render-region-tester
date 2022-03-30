54c67cf99b498bcb20db6616cee9de83be9cad68
John Beynon <john@render.com>
2022-03-30 10:32:47 +0100

    Add RENDER so it's obvious

--- tester/src/App.js
+++ tester/src/App.js
@@ -8,20 +8,20 @@ function App() {
   return (
     <div className="p-6">
       <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-2">
-          <Chart region="Frankfurt (rewrite)" url="/frankfurt" />
-          <Chart region="Frankfurt (direct)" url="https://test-frankfurt.onrender.com" />
+          <Chart region="Frankfurt RENDER (rewrite)" url="/frankfurt" />
+          <Chart region="Frankfurt RENDER (direct)" url="https://test-frankfurt.onrender.com" />
           <Chart region="Frankfurt NETLIFY (rewrite)" url="https://extraordinary-chaja-86c0d6.netlify.app/frankfurt" />
           <Chart region="Frankfurt VERCEL (rewrite)" url="https://vercel-rewrite-rho.vercel.app/frankfurt" />        
-          <Chart region="Ohio (rewrite)" url="/ohio" />
-          <Chart region="Ohio (direct)" url="https://test-ohio.onrender.com" />
+          <Chart region="Ohio RENDER (rewrite)" url="/ohio" />
+          <Chart region="Ohio RENDER (direct)" url="https://test-ohio.onrender.com" />
           <Chart region="Ohio NETLIFY (rewrite)" url="https://extraordinary-chaja-86c0d6.netlify.app/ohio" />
           <Chart region="Ohio VERCEL (rewrite)" url="https://vercel-rewrite-rho.vercel.app/ohio" />
-          <Chart region="Oregon (rewrite)" url="/oregon" />
-          <Chart region="Oregon (direct)" url="https://test-oregon.onrender.com" />
+          <Chart region="Oregon RENDER (rewrite)" url="/oregon" />
+          <Chart region="Oregon RENDER (direct)" url="https://test-oregon.onrender.com" />
           <Chart region="Oregon NETLIFY (rewrite)" url="https://extraordinary-chaja-86c0d6.netlify.app/oregon" />
           <Chart region="Oregon VERCEL (rewrite)" url="https://vercel-rewrite-rho.vercel.app/oregon" />
-          <Chart region="Singapore (rewrite)" url="/singapore" />
-          <Chart region="Singapore (direct)" url="https://test-singapore-ak20.onrender.com" />
+          <Chart region="Singapore RENDER (rewrite)" url="/singapore" />
+          <Chart region="Singapore RENDER (direct)" url="https://test-singapore-ak20.onrender.com" />
           <Chart region="Singpore NETLIFY (rewrite)" url="https://extraordinary-chaja-86c0d6.netlify.app/singapore" />
           <Chart region="Singapore VERCEL (rewrite)" url="https://vercel-rewrite-rho.vercel.app/singapore" />
            </ul>
